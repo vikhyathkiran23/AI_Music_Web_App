9197af3765520113f373a713ab20c4aad108d388
vikhyathkiran23 <72803191+vikhyathkiran23@users.noreply.github.com>
2022-08-18 20:23:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 song_1="";
 song_2="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
+    song_1 = loadSound("music.mp3");
+    song_2 = loadSound("music2.mp3");
     
 }
 function setup(){
@@ -10,6 +16,28 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, ModelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X= " + leftWristX + " Left Wrist Y= " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X= " + rightWristX + " Right Wrist Y= " + rightWristY);
+    }
+
+
+}
+
+function ModelLoaded() {
+    console.log("PoseNet is initialized");
 }
 
 function draw(){
